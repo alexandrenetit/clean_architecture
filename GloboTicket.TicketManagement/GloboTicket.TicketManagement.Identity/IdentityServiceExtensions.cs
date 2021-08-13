02bbd8bbe00e6cbceb53ec5fdf63698e9778ab3e
Alexandre Alves Goncalves <a.alves.goncalves@avanade.com>
2021-08-13 10:19:56 -0300

    Add handling erros in API

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Identity/IdentityServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Identity
+{
+   public static class IdentityServiceExtensions
+    {
+        public static void AddIdentityServices(this IServiceCollection services, IConfiguration configuration )
+        {
+
+        }
+    }
+}
